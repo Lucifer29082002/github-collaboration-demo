d836b724b7a3e30cec3052aa18b8be9ce2883dcd
Lucifer29082002 <ranaakhil553@gmail.com>
2025-06-16 06:36:23 -0400

    Add reverse option to greet function

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def greet(name):
-    return f"Hello, {name}!"
+def greet(name, reverse=False):
+    message = f"Hello, {name}!"
+    return message[::-1] if reverse else message
 
 print(greet("World"))
+print(greet("World", reverse=True))
+
